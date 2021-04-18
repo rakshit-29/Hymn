fe18d92d5dbc0314f9543eb3fdbd0796f700b93a
Rakshit <rakshit290900@gmail.com>
2021-04-18 16:19:14 -0400

    "Login Activity committing"

---
+++ app/src/main/java/com/example/hymn/activities/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.hymn.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.hymn.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        // It is deprecated in the API level 30. I will update you with the alternate solution soon.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+    }
+}
--- app/src/main/java/com/example/hymn/activities/MainActivity.kt
+++ app/src/main/java/com/example/hymn/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hymn
+package com.example.hymn.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hymn.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
